709d571f33b79b03038ff21e6107de64b75428fc
kddinev18 <kddinev18@codingburgas.bg>
2020-11-22 13:32:13 +0200

    Create mainMenu function

--- TheBletchleyGames/TheBletchleyGames/TheBletchleyGames.cpp
+++ TheBletchleyGames/TheBletchleyGames/TheBletchleyGames.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
+bool mainMenu()
+{
+    int option;
+    cin >> option;
+    switch (option)
+    {
+    case 1:
+
+        return true;
+        break;
+
+    case 2:
+
+        return false;
+        break;
+
+    default:
+
+        mainMenu();
+        break;
+
+    }
+}
+
 int main()
 {
-
+    bool exit = true;
+    do
+    {
+        exit = mainMenu();
+    } while (exit);
 }
